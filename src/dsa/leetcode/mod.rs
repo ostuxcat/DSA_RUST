0c2486131b85dbad99a526f28065bd5f4c0c60ce
ostuxcat <ostuxcat@ostuxcat.in>
2026-01-21 23:35:15 +0530

    LeetCode: Problem No. 509 The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1.

--- src/dsa/leetcode/mod.rs
+++ src/dsa/leetcode/mod.rs
@@ -6,8 +6,8 @@ pub fn problem_136(nums: Vec<i32>) -> i32 {
     return ans;
 }
 pub fn problem_509(n: i32) -> i32 {
-    if (n == 0 || n == 1) {
-        return n;
+    match n {
+        0 | 1 => return n,
+        _ => return problem_509(n - 1) + problem_509(n - 2),
     }
-    return problem_509(n - 1) + problem_509(n - 2);
 }
